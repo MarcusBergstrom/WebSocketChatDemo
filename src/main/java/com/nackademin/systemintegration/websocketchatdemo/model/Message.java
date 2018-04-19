4bf42ae9c78117a7498732288103fe424cad9148
Marcus Bergström <marcusbergstrompt@gmail.com>
2018-04-19 14:08:19 +0200

    WebSocketChatDemo

---
+++ src/main/java/com/nackademin/systemintegration/websocketchatdemo/model/Message.java
@@ -0,0 +1,20 @@
+package com.nackademin.systemintegration.websocketchatdemo.model;
+
+public class Message {
+    
+    private String deviceState;
+    
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+    public String getDeviceState(){
+        return deviceState;
+    }
+    
+    public void setDeviceState(String deviceState) {
+        this.deviceState = deviceState;
+    }
+    
+}
